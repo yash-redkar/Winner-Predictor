0c3904d8aff9a1cfdd270c7d931974fa22ae430e
Yash Redkar <redkar19yash@gmail.com>
2025-10-27 23:08:47 +0530

    updated logic

--- src/algorithms/twoScan.js
+++ src/algorithms/twoScan.js
@@ -5,13 +5,16 @@ export const runTwoScan = (players) => {
 
   let maxScore = players[0].score;
   let secondMaxScore = -Infinity;
+  let maxId = players[0].id;
 
   // Single pass to find max and second max
   for (let i = 1; i < players.length; i++) {
     const cmp = counter.compare(players[i].score, maxScore);
-    if (cmp > 0) {
+    if (cmp > 0 || (cmp === 0 && players[i].id < maxId)) {
+      // New winner or tie (smaller ID wins tie)
       secondMaxScore = maxScore;
       maxScore = players[i].score;
+      maxId = players[i].id; // store ID for tie-break
     } else if (cmp < 0) {
       const cmp2 = counter.compare(players[i].score, secondMaxScore);
       if (cmp2 > 0) {
